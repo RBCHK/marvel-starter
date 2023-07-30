8b0ee53c2113068f1d3efd6cde27f2e43e6f1db6
Artem Rybachuk <artem.rybachuk@gmail.com>
2023-07-30 13:54:27 +0300

    CharInfo component implemented

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import MarvelService from '../../services/MarvelService';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Spinner from '../spinner/Spinner';
+
 import './charList.scss';
 
 class CharList extends Component {
@@ -19,10 +22,9 @@ class CharList extends Component {
 		this.marvelService
 			.getAllCharacters()
 			.then(response => {
-				this.setState({ characters: response });
+				this.setState({ characters: response, loading: false });
 			})
 			.catch(() => {
-				console.log('error while all characters updating');
 				this.setState({
 					loading: false,
 					error: true,
@@ -31,36 +33,58 @@ class CharList extends Component {
 	};
 
 	render() {
-		const { characters } = this.state;
+		const { characters, loading, error } = this.state;
+		const { onCharSelected } = this.props;
+
+		const errorMessage = error ? <ErrorMessage /> : null;
+		const spinner = loading ? <Spinner /> : null;
+		const content =
+			loading || error || !characters ? null : (
+				<View characters={characters} onCharSelected={onCharSelected} />
+			);
 
 		return (
 			<div className='char__list'>
-				<ul className='char__grid'>
-					{characters.map(character => {
-						const imgNotAvailable =
-							character.thumbnail ===
-							'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
-								? { objectFit: 'unset' }
-								: { objectFit: 'cover' };
-
-						return (
-							<li key={character.id} className='char__item'>
-								<img
-									src={character.thumbnail}
-									alt={character.name}
-									style={imgNotAvailable}
-								/>
-								<div className='char__name'>{character.name}</div>
-							</li>
-						);
-					})}
-				</ul>
-				<button className='button button__main button__long'>
-					<div className='inner'>load more</div>
-				</button>
+				{errorMessage}
+				{spinner}
+				{content}
 			</div>
 		);
 	}
 }
+
+const View = ({ characters, onCharSelected }) => {
+	return (
+		<>
+			<ul className='char__grid'>
+				{characters.map(character => {
+					const imgNotAvailable =
+						character.thumbnail ===
+						'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+							? { objectFit: 'unset' }
+							: { objectFit: 'cover' };
+
+					return (
+						<li
+							key={character.id}
+							className='char__item'
+							onClick={() => onCharSelected(character.id)}
+						>
+							<img
+								src={character.thumbnail}
+								alt={character.name}
+								style={imgNotAvailable}
+							/>
+							<div className='char__name'>{character.name}</div>
+						</li>
+					);
+				})}
+			</ul>
+			<button className='button button__main button__long'>
+				<div className='inner'>load more</div>
+			</button>
+		</>
+	);
+};
 
 export default CharList;
